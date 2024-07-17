4861e8cc475fb4dda7ccb82cd8974991dbe6c8f7
SonDev <son.phamthanh175@gmail.com>
2024-07-18 00:53:39 +0700

    config (cart component ) : render item cart by cartApi

---
+++ front-end/src/pages/AccountInfo/components/AccountAdditional.jsx
@@ -0,0 +1,16 @@
+import { Paper } from '@material-ui/core'
+import React from 'react'
+
+function AccountAdditional(props) {
+  return (
+    <Paper elevation={0} style={{
+      padding: '15px',
+  }}>
+    <div>Thông tin vận chuyển </div>
+  </Paper>
+  )
+}
+
+AccountAdditional.propTypes = {}
+
+export default AccountAdditional
